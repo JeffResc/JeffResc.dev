60ca5a3ee769046004dfec1135e06f90197e2ce7
Jeff Rescignano <jeff@jeffresc.dev>
2022-04-11 11:13:22 -0400

    Add build date and time to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,13 @@ import React from "react";
 import { useStaticQuery, graphql } from 'gatsby'
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    {
+      currentBuildDate {
+        currentDate
+      }
+    }
+  `)
   return (
     <footer className="px-4 py-8 bg-coolGray-800 text-white">
       <div className="container flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
@@ -10,7 +17,7 @@ const Footer = () => {
           ©️ 2020-{new Date().getFullYear()} JeffResc
         </div>
         <div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
-          Last Updated: {data.currentBuildDate.currentDate}
+          Last Generated: {data.currentBuildDate.currentDate}
         </div>
       </div>
     </footer>
@@ -18,11 +25,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-const data = useStaticQuery(graphql`
-  query {
-    currentBuildDate {
-      currentDate
-    }
-  }
-`);
