c9f8568241344ae01fba61138257dcd28cf11f27
Jeff Rescignano <jeff@jeffresc.dev>
2022-04-11 11:32:39 -0400

    Add relative build date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
 import React from "react";
 
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
 import { useStaticQuery, graphql } from 'gatsby'
+
+dayjs.extend(relativeTime);
 
 const Footer = () => {
   const data = useStaticQuery(graphql`
@@ -10,14 +15,17 @@ const Footer = () => {
       }
     }
   `)
+
+  const buildDate = dayjs(data.currentBuildDate.currentDate);
+
   return (
     <footer className="px-4 py-8 bg-coolGray-800 text-white">
       <div className="container flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
         <div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
-          ©️ 2020-{new Date().getFullYear()} JeffResc
+          ©️ 2020-{buildDate.year()} JeffResc
         </div>
         <div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
-          Last Generated: {data.currentBuildDate.currentDate}
+          <span title={buildDate.toString()}>Last built {buildDate.fromNow()}</span>
         </div>
       </div>
     </footer>
