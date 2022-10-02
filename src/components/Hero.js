415c43e0fa6c05eccec4ed0bbb3ac7036bac4307
Jeff Rescignano <jeff@jeffresc.dev>
2022-10-02 18:04:38 -0400

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ function if_subtitle(subtitle) {
   if (subtitle !== "false") {
     return (
       <p className="mt-8 text-xl text-white">
-        I'm a Junior at Clarkson University studying Computer Science.
+        I'm a Senior at Clarkson University studying Computer Science with a dual minor in Mathematics and Communication.
       </p>
     )
   }
